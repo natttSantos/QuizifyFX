f52a5f27aa745654a8b07501ef27b32b60c29ca3
Cristobal Vidal Orquin <crvidorq@upv.edu.es>
2022-03-10 11:02:17 +0100

    Clase respuesta

---
+++ src/controlador/GenerarQuizNoAleatorioController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author crivi
+ */
+public class GenerarQuizNoAleatorioController implements Initializable {
+
+    @FXML
+    private ListView<?> lista;
+    @FXML
+    private TextField buscador;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void crearQuizz(ActionEvent event) {
+    }
+    
+}
